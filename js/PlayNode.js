e622afd08af56ee79736d1d8d1c1d8bdc878c48d
James Baker <j_d_baker@hotmail.com>
2015-02-09 15:31:43 +0000

    PlayNode Fix

--- js/PlayNode.js
+++ js/PlayNode.js
@@ -1,8 +1,8 @@
 function PlayNode(parent, childNumber) {
-	var note = new ValueNode('note', this, 0, 60, 40, 100);
-	var amp = new ValueNode('amp', this, 1, 0, 1);
-	var release = new ValueNode('release', this, 1, 0, 5);
-	var playNode = new ActionNode('play', parent, childNumber, [note, amp, release]);
+	var playNode = new ActionNode('play', parent, childNumber);
+	ValueNode('note', playNode, 0, 60, 40, 100);
+	ValueNode('amp', playNode, 1, 1, 0, 1);
+	ValueNode('release', playNode, 2, 1, 0, 5);
 	
 	return playNode;
 }
