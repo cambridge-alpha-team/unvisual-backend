84b3a9612d2a98dc7467f6a17a4c50b3b9d5d3f5
Isaac Dunn <ird28@cam.ac.uk>
2015-02-09 17:32:21 +0000

    Started work on OSC stuff
    Written a class that can send OSC messages, and another that can receive
    them to check that it works. I haven't been able to get it to work with
    the Sonic Pi server, though.

---
+++ java-backend/src/uk/ac/cam/alpha/osc/OscTestReceiver.java
@@ -0,0 +1,29 @@
+package uk.ac.cam.alpha.osc;
+
+import java.net.SocketException;
+import java.util.Date;
+
+import com.illposed.osc.OSCListener;
+import com.illposed.osc.OSCMessage;
+import com.illposed.osc.OSCPortIn;
+
+/**
+ * @author Isaac Dunn &lt;ird28@cam.ac.uk&gt;
+ */
+public class OscTestReceiver implements OSCListener {
+
+    @Override
+    public void acceptMessage(Date time, OSCMessage message) {
+        System.out.println("MESSAGE RECEIVED");
+        System.out.println("Address: " + message.getAddress());
+        System.out.println("Message: " + message.getArguments()[0]);
+    }
+    
+    public static void main(String[] args) throws SocketException {
+        OSCPortIn in = new OSCPortIn(12345);
+        in.addListener("/run-code", new OscTestReceiver());
+        System.out.println("Starting listener");
+        in.startListening();
+    }
+
+}
