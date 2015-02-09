483c8d5d51d631c653b9370017fae7ccfc54c41c
James Baker <j_d_baker@hotmail.com>
2015-02-09 15:11:38 +0000

    playNode now has values

--- js/PlayNode.js
+++ js/PlayNode.js
@@ -1,5 +1,8 @@
 function PlayNode(parent, childNumber) {
-	var playNode = new ActionNode('play', parent, childNumber, [/* a list of ValueNodes */]);
+	var note = new ValueNode('note', this, 0, 60, 40, 100);
+	var amp = new ValueNode('amp', this, 1, 0, 1);
+	var release = new ValueNode('release', this, 1, 0, 5);
+	var playNode = new ActionNode('play', parent, childNumber, [note, amp, release]);
 	
 	return playNode;
 }
