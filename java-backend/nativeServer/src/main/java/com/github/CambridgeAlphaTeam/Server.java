7224985e74aac0d553108206edd2aede1685eead
Kovacsics Robert (Arch) <kovirobi@gmail.com>
2015-02-17 12:42:22 +0000

    Maven-ified nativeServer

--- java-backend/nativeServer/src/main/java/com/github/CambridgeAlphaTeam/Server.java
+++ java-backend/nativeServer/src/main/java/com/github/CambridgeAlphaTeam/Server.java
@@ -10,7 +10,7 @@ import com.sun.net.httpserver.*;
  * @author Isaac Dunn &lt;ird28@cam.ac.uk&gt;
  */
 public class Server {
-    
+
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/hiworld", new ExampleHandler());
@@ -18,7 +18,7 @@ public class Server {
         server.start();
         System.out.println("Server started");
     }
-    
+
     static class ExampleHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
             String response = "Hello World!";
