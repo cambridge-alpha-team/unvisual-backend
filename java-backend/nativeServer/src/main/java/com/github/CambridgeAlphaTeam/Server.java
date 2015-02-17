ec70a082ad57c2e4ceee65ededf472cd6492a058
Isaac Dunn <ird28@cam.ac.uk>
2015-02-17 13:07:44 +0000

    Now handles run code and stop music requests Not sure that it actually works - to be tested

--- java-backend/nativeServer/src/main/java/com/github/CambridgeAlphaTeam/Server.java
+++ java-backend/nativeServer/src/main/java/com/github/CambridgeAlphaTeam/Server.java
@@ -1,19 +1,29 @@
 package com.github.CambridgeAlphaTeam;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-import com.sun.net.httpserver.*;
+import com.github.cambridgeAlphaTeam.OscException;
+import com.github.cambridgeAlphaTeam.OscSender;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 /**
  * @author Isaac Dunn &lt;ird28@cam.ac.uk&gt;
  */
 public class Server {
 
+    private static OscSender sender;
+
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/hiworld", new ExampleHandler());
+        server.createContext("/run", new RunCodeHandler());
+        server.createContext("/stop", new StopMusicHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
         System.out.println("Server started");
@@ -26,6 +36,35 @@ public class Server {
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
+        }
+    }
+
+    static class RunCodeHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(t.getRequestBody()));
+            StringBuilder strb = new StringBuilder();
+            while (br.ready()) {
+                strb.append(br.readLine()); // not sure this is the best way of doing this?
+            }
+            try {
+                sender.sendCode(strb.toString());
+            } catch (OscException e) {
+                e.printStackTrace();
+                throw new IOException("Sending code to Sonic Pi server failed", e);
+            }
+            t.sendResponseHeaders(200, 0);
+        }
+    }
+
+    static class StopMusicHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            try {
+                sender.stopAll();
+            } catch (OscException e) {
+                e.printStackTrace();
+                throw new IOException("Sending message to Sonic Pi server failed", e);
+            }
+            t.sendResponseHeaders(200, 0);
         }
     }
 
