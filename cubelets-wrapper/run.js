0f3be967787fa9d4e99159e743b6e61a4389cb71
blob8108 <blob8108@gmail.com>
2015-02-17 11:21:49 +0000

    Scan for cubelets rather than hardcoding path

--- cubelets-wrapper/run.js
+++ cubelets-wrapper/run.js
@@ -4,96 +4,99 @@ var BlockValueEventResponse = require('./node-cubelets/response/blockValueEvent.
 
 // Connecting to Cubelet
 
-var connection = new cubelets.SerialConnection({
-  path: '/dev/cu.Cubelet-RYB-AMP-SPP',  // hardcoded path!
-});
+var scanner = new cubelets.BluetoothScanner('Cubelet-RYB');
 
-connection.on('open', function() {
-  console.error('Connection open');
-});
+scanner.scan();
 
-connection.on('close', function() {
-  console.error('Connection closed');
-});
+scanner.on('pass', function(connection, name, config) {
 
-console.error('Connecting...');
-connection.open();
+  connection.on('open', function() {
+    console.error('Connection open');
+  });
 
-var construction = new cubelets.Construction(connection);
+  connection.on('close', function() {
+    console.error('Connection closed');
+  });
 
-construction.connect(function() {
-  console.error('Construction connected, sending discover()');
-  construction.discover();
-});
+  console.error('Connecting...');
 
 
-// When construction changes, subscribe to value events from each new block
+  var construction = new cubelets.Construction(connection);
 
-function pretty(x) {
-  if (x.type) {
-    return {id: x.id, type: x.type};
-  } else if (x.length) {
-    return x.map(pretty);
-  } else {
-    return x;
+  construction.connect(function() {
+    console.error('Construction connected, sending discover()');
+    construction.discover();
+  });
+
+
+  // When construction changes, subscribe to value events from each new block
+
+  function pretty(x) {
+    if (x.type) {
+      return {id: x.id, type: x.type};
+    } else if (x.length) {
+      return x.map(pretty);
+    } else {
+      return x;
+    }
   }
-}
 
-construction.on('change', function() {
-  process.stderr.write("\u001b[2J\u001b[0;0H"); // clear the terminal
-  console.error('Construction changed:');
-  console.error('The origin is \n', pretty(construction.origin()), '\n');
-  console.error('The direct neighbors are near \n', pretty(construction.near()), '\n');
-  console.error('The other cubelets are far \n', pretty(construction.far()), '\n');
+  construction.on('change', function() {
+    process.stderr.write("\u001b[2J\u001b[0;0H"); // clear the terminal
+    console.error('Construction changed:');
+    console.error('The origin is \n', pretty(construction.origin()), '\n');
+    console.error('The direct neighbors are near \n', pretty(construction.near()), '\n');
+    console.error('The other cubelets are far \n', pretty(construction.far()), '\n');
 
-  construction.all().forEach(function(cubelet) {
-    var id = cubelet.id;
-    if (!values.hasOwnProperty(id)) {
-      values[id] = null;
-      grabCubelets.push(id);
+    construction.all().forEach(function(cubelet) {
+      var id = cubelet.id;
+      if (!values.hasOwnProperty(id)) {
+        values[id] = null;
+        grabCubelets.push(id);
 
+        var command = new cubelets.RegisterBlockValueEventCommand(id);
+        connection.postCommand(command);
+      }
+    });
+
+    lastChange = new Date();
+    sent = false;
+  });
+
+
+  // When we receive a value, work out which cubelet it was,
+  // and output updated values dict
+
+  var values = {};
+  var grabCubelets = [];
+
+  construction.on('value', function(response) {
+    if (response.type.code === 'b') {
+      var r = new BlockValueEventResponse(response.data, 'b');
+
+      var index = grabCubelets.indexOf(r.id);
+      if (index !== -1) {
+        grabCubelets.splice(index, 1);
+      }
+
+      values[r.id] = r.value;
+      console.log(values);
+    }
+  });
+
+
+  // If subscribing to a block failed the first time, try it again.
+  // This never works!
+
+  setInterval(function() {
+    if (!connection.isOpen()) return;
+
+    grabCubelets.forEach(function(id) {
       var command = new cubelets.RegisterBlockValueEventCommand(id);
       connection.postCommand(command);
-    }
-  });
+      console.error("grabbing " + id);
+    });
+  }, 1000);
 
-  lastChange = new Date();
-  sent = false;
 });
-
-
-// When we receive a value, work out which cubelet it was,
-// and output updated values dict
-
-var values = {};
-var grabCubelets = [];
-
-construction.on('value', function(response) {
-  if (response.type.code === 'b') {
-    var r = new BlockValueEventResponse(response.data, 'b');
-
-    var index = grabCubelets.indexOf(r.id);
-    if (index !== -1) {
-      grabCubelets.splice(index, 1);
-    }
-
-    values[r.id] = r.value;
-    console.log(values);
-  }
-});
-
-
-// If subscribing to a block failed the first time, try it again.
-// This never works!
-
-setInterval(function() {
-  if (!connection.isOpen()) return;
-
-  grabCubelets.forEach(function(id) {
-    var command = new cubelets.RegisterBlockValueEventCommand(id);
-    connection.postCommand(command);
-    console.error("grabbing " + id);
-  });
-}, 1000);
-
 
