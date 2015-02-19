47b6b4a859ef5d5a19fe9f2cfaa02fa0654bd5bc
Isaac Dunn <ird28@cam.ac.uk>
2015-02-19 12:50:43 +0000

    Server logs & actually sends OSC messages Added logging and fixed lots of stupid mistakes

--- java-backend/nativeServer/src/main/java/com/github/CambridgeAlphaTeam/Server.java
+++ java-backend/nativeServer/src/main/java/com/github/CambridgeAlphaTeam/Server.java
@@ -8,9 +8,13 @@ import java.net.InetSocketAddress;
 
 import com.github.cambridgeAlphaTeam.OscException;
 import com.github.cambridgeAlphaTeam.OscSender;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Isaac Dunn &lt;ird28@cam.ac.uk&gt;
@@ -18,16 +22,18 @@ import com.sun.net.httpserver.HttpServer;
 public class Server {
 
     private static OscSender sender;
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, OscException {
         if (args.length != 1) {
             System.err.println("Give precisely one argument, the location of the front end files to serve");
             return;
         }
+        sender = new OscSender();
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/hiworld", new LoggerHandler(new ExampleHandler()));
-        server.createContext("/run", new LoggerHandler(new RunCodeHandler()));
-        server.createContext("/stop", new LoggerHandler(new StopMusicHandler()));
+        server.createContext("/rest/osc/run", new LoggerHandler(new RunCodeHandler()));
+        server.createContext("/rest/osc/stop", new LoggerHandler(new StopMusicHandler()));
         server.createContext("/", new LoggerHandler(new FileHandler(args[0]))); // serves front end
         server.setExecutor(null); // creates a default executor
         server.start();
@@ -46,15 +52,16 @@ public class Server {
 
     static class RunCodeHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
+            logger.debug("Request to run code received");
             BufferedReader br = new BufferedReader(new InputStreamReader(t.getRequestBody()));
             StringBuilder strb = new StringBuilder();
             while (br.ready()) {
-                strb.append(br.readLine()); // not sure this is the best way of doing this?
+                strb.append(br.readLine() + "\n"); // not sure this is the best way of doing this?
             }
             try {
+                logger.debug("Sending code to Sonic Pi:\n" + strb.toString());
                 sender.sendCode(strb.toString());
             } catch (OscException e) {
-                e.printStackTrace();
                 throw new IOException("Sending code to Sonic Pi server failed", e);
             }
             t.sendResponseHeaders(200, 0);
@@ -63,10 +70,11 @@ public class Server {
 
     static class StopMusicHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
+            logger.debug("Request to stop music received");
             try {
+                logger.debug("Sending stop request to Sonic Pi");
                 sender.stopAll();
             } catch (OscException e) {
-                e.printStackTrace();
                 throw new IOException("Sending message to Sonic Pi server failed", e);
             }
             t.sendResponseHeaders(200, 0);
